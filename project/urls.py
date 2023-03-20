92a8bcf8b3fe26ad80ee6dc6b0d82b6123e82f31
Said Azmour <azmour2016maroc@gmail.com>
2023-03-20 21:16:23 +0100

    edit urls

--- project/urls.py
+++ project/urls.py
@@ -22,7 +22,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', include('blog.urls',namespace='blog')),
+    path('/', include('blog.urls',namespace='blog')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('admin/', admin.site.urls),
