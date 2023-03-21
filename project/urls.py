623d9822c792a4622fdbcc781f47da47eb4043ae
Said Azmour <azmour2016maroc@gmail.com>
2023-03-21 01:21:20 +0100

    add collectstatic

--- project/urls.py
+++ project/urls.py
@@ -17,10 +17,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from . import views
-
-handler404 = views.handler404
-handler500 = views.handler500
 
 
 
