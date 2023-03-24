82978a96afdec3c86cc05dfb84b96dbbeef454b4
Said Azmour <azmour2016maroc@gmail.com>
2023-03-24 16:57:47 +0100

    idit courses

--- project/settings.py
+++ project/settings.py
@@ -188,8 +188,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 FAVICON_PATH = STATIC_URL + 'images/favicon.ico'
 
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
