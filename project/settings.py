dc42915f4367dca2f353dc61d8dc8222e754a344
Said Azmour <azmour2016maroc@gmail.com>
2023-03-25 22:46:46 +0100

    add s3

--- project/settings.py
+++ project/settings.py
@@ -231,6 +231,8 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_LOCATION = 'media'
+AWS_DEFAULT_ACL = 'public-read'
+AWS_QUERYSTRING_AUTH = False
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
