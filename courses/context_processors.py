409921598089199c849d4acfdc95b7dab45abca3
Said Azmour <azmour2016maroc@gmail.com>
2023-03-28 16:23:29 +0200

    edit footer

--- courses/context_processors.py
+++ courses/context_processors.py
@@ -1,6 +1,7 @@
 from .models import Course
 
 def courses(request):
+    courses = Course.objects.all()
     l_courses = Course.objects.all()[0:3]
     
-    return {'l_courses': l_courses}
+    return {'l_courses': l_courses, 'courses': courses}
