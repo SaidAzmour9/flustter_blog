937e987ffbf1204961c11f16338a6057552cf32d
Said Azmour <azmour2016maroc@gmail.com>
2023-04-10 17:22:56 +0200

    add admin panel

--- courses/admin.py
+++ courses/admin.py
@@ -2,12 +2,6 @@ from django.contrib import admin
 from .models import Course, Lesson
 # Register your models here.
 
-class LessonAdmin(admin.ModelAdmin):
-    list_display = ('title', 'course')
-    list_filter = (
-        ('course', admin.RelatedOnlyFieldListFilter),
-    )
 
 admin.site.register(Course)
 admin.site.register(Lesson)
-admin.site.register(LessonAdmin)
